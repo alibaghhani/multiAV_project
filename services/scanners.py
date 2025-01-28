2e6c746e0be92bed9caa3e56f282b6dc56644e1d
alibaghani <baghaniali2006@gmail.com>
2025-01-28 16:17:15 -0500

    feat: create MetaDefender service

--- services/scanners.py
+++ services/scanners.py
@@ -57,3 +57,37 @@ class Kaspersky(AbstractAntivirus):
                     file_id=None):
         return requests.get(self.RESULTS_URL + f'/{file_id}',
                             headers=self.set_header()).json()
+
+
+
+class MetaDefender(AbstractAntivirus):
+    UPLOAD_URL = 'https://www.opswat.com/api/scan/file'
+    RESULTS_URL = 'https://www.opswat.com/api/scan/'
+
+    def upload_file(self):
+        response = requests.post(
+            self.UPLOAD_URL,
+            files={'file': self._file},
+            headers=self.set_header()
+        )
+
+        if response.status_code == 200:
+            json_response = response.json()
+            self._file_id = json_response['flow_id']
+            print(f"Uploaded file ID: {self._file_id}")
+        else:
+            raise Exception(f"File upload failed with status code {response.status_code}: {response.text}")
+
+    def get_results(self):
+        if not self._file_id:
+            raise ValueError("File ID is not set, cannot fetch results.")
+
+        response = requests.get(
+            self.RESULTS_URL + self._file_id + '/report',
+            headers=self.set_header()
+        )
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception(f"Failed to get results with status code {response.status_code}: {response.text}")
