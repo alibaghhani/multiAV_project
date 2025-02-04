65089eaebb8ea015ba1c6c9185674b8c33562186
alibaghani <baghaniali2006@gmail.com>
2025-02-04 05:46:40 -0500

    feat: create ScanFile model

--- scanner/models.py
+++ scanner/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+from core.models import CommonItems
+
+
+class ScanFile(CommonItems):
+    file = models.FileField()
+    sha_256 = models.CharField(max_length=64, unique=True)
+    file_size = models.BigIntegerField()
+    status = models.IntegerField(choices=CommonItems.RESULT_CHOICES)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.sha_256}-----{self.name}'
+
