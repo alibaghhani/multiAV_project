eeeeef5282680d2569f584704746b52a499a9068
alibaghani <baghaniali2006@gmail.com>
2025-02-04 05:47:38 -0500

    chore: change database to postgresql

--- multiav_project/settings.py
+++ multiav_project/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'multiav_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'multiav',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -124,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = 'media/'
