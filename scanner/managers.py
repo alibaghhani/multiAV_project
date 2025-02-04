703fc72b235cc4d4e8167589c12c8a4e925e7244
alibaghani <baghaniali2006@gmail.com>
2025-02-04 07:08:58 -0500

    chore: optimizing imports

--- scanner/managers.py
+++ scanner/managers.py
@@ -1,20 +1,11 @@
-from django.db import models
 from core.utilities import calculate_file_hash, get_file_size
 from django.conf import settings
-import hashlib
-import io
-
+from django.db import models
 
 
 class ScanFileManager(models.Manager):
     def create(self, **kwargs):
         kwargs['sha_256'] = calculate_file_hash(str(kwargs['file']))
         kwargs['name'] = str(kwargs['file'])
-        kwargs['file_size'] = get_file_size(settings.MEDIA_ROOT+str(kwargs['file']))
+        kwargs['file_size'] = get_file_size(settings.MEDIA_ROOT + str(kwargs['file']))
         return super().create(**kwargs)
-
-
-
-
-
-
