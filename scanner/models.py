541964cb36bf8e38a3956572d2a62eb3904d1996
alibaghani <baghaniali2006@gmail.com>
2025-02-04 05:47:08 -0500

    feat: create Scan model for saving scan results

--- scanner/models.py
+++ scanner/models.py
@@ -16,3 +16,23 @@ class ScanFile(CommonItems):
     def __str__(self):
         return f'{self.sha_256}-----{self.name}'
 
+class Scan(CommonItems):
+
+    STATUS_CHOICES = (
+        (1, 'NOT_UPLOADED'),
+        (2, 'PENDING'),
+        (3, 'SCANNED'),
+        (4, 'NOTIFIED')
+    )
+
+
+    status = models.IntegerField(choices=STATUS_CHOICES)
+    av_name = models.CharField(max_length=255, unique=True)
+    file = models.ForeignKey('ScanFile', on_delete=models.CASCADE, related_name='scan')
+    tracking_id = models.BigIntegerField()
+    short_result = models.IntegerField(choices=CommonItems.RESULT_CHOICES)
+    final_result = models.JSONField()
+
+    def __str__(self):
+        return f"Tracking ID: {self.tracking_id}"
+
