d1666e0bc206f439cca7aad873dc15fcc4c2f3b4
alibaghani <baghaniali2006@gmail.com>
2025-02-04 06:50:17 -0500

    feat: create function to calculate file hash

---
+++ core/utilities.py
@@ -0,0 +1,13 @@
+import hashlib
+import io
+from pathlib import Path
+
+
+def calculate_file_hash(file_path: str) -> str:
+    with open(file_path, 'rb') as file:
+        file_obj = io.BytesIO(file.read())
+        hash_ = hashlib.file_digest(file_obj, 'sha256').hexdigest()
+    return hash_
+
+
+
