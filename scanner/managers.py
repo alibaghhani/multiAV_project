77b629dbbb4c47ffecd35a07809e183c9c8b9c69
alibaghani <baghaniali2006@gmail.com>
2025-02-05 07:09:36 -0500

    feat: change scanfile manager to calculate hash before saving file

--- scanner/managers.py
+++ scanner/managers.py
@@ -1,20 +1,14 @@
-from core.utilities import calculate_file_hash, get_file_size
+from core.utilities import get_file_size, calculate_file_hash
 from django.conf import settings
 from django.db import models
 
 
 class ScanFileManager(models.Manager):
     def create(self, **kwargs):
-        kwargs['name'] = str(kwargs['file'])
+        file = kwargs['file']
 
+        kwargs['name'] = str(file)
+        kwargs['sha_256'] = calculate_file_hash(file)
+        kwargs['file_size'] = file.size
 
-        instance = super().create(**kwargs)
-
-        file_path = instance.file.path
-
-        instance.sha_256 = calculate_file_hash(file_path)
-
-        instance.file_size = get_file_size(file_path)
-
-        instance.save(update_fields=['sha_256', 'name', 'file_size'])
-        return instance
+        return super().create(**kwargs)
