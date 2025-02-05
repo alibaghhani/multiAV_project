ce78ff28163416e1e53394334943c411a949da72
alibaghani <baghaniali2006@gmail.com>
2025-02-05 09:58:21 -0500

    feat: add some functionalities to scanfile serializer and change its name

--- scanner/serializers.py
+++ scanner/serializers.py
@@ -1,9 +1,41 @@
-from rest_framework.serializers import ModelSerializer
+from urllib.parse import urlparse
+
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+from rest_framework import serializers
+
+from core.utilities import calculate_file_hash
 from .models import ScanFile
 
-class ScanFileSerializer(ModelSerializer):
+
+class ScanFileCreateSerializer(serializers.ModelSerializer):
+    file_hash = serializers.SerializerMethodField()
+    link = serializers.SerializerMethodField()
+
+
     class Meta:
         model = ScanFile
-        fields = ('file', )
+        fields = 'file', 'file_hash', 'link'
 
 
+    def create(self, validated_data):
+
+
+        file = validated_data.get('file')
+
+        file_hash = calculate_file_hash(file)
+
+        try:
+            return get_object_or_404(ScanFile, sha_256=file_hash)
+        except Http404:
+            scan_file = ScanFile.objects.create(**validated_data)
+            return scan_file
+
+    @staticmethod
+    def get_file_hash(obj):
+        return obj.sha_256
+
+    @staticmethod
+    def get_link(obj):
+        link = urlparse('http://127.0.0.1:8000/api/file/')
+        return link.path + obj.sha_256
