3dc5468542981c1471db29dcfe1994b70730bde9
alibaghani <baghaniali2006@gmail.com>
2025-02-05 07:08:13 -0500

    feat: change calculate file hash util to calculate hsh before saving file

--- core/utilities.py
+++ core/utilities.py
@@ -1,15 +1,14 @@
 import hashlib
-import io
 from pathlib import Path
 
 
-def calculate_file_hash(file_path: str) -> str:
-    with open(file_path, 'rb') as file:
-        file_obj = io.BytesIO(file.read())
-        hash_ = hashlib.file_digest(file_obj, 'sha256').hexdigest()
-    return hash_
+def calculate_file_hash(uploaded_file)->str:
+    hash_obj = hashlib.sha256()
 
+    for chunk in uploaded_file.chunks():
+        hash_obj.update(chunk)
 
+    return hash_obj.hexdigest()
 
-def get_file_size(file_path: str)->int:
+def get_file_size(file_path: str) -> int:
     return Path(file_path).stat().st_size
