a80c3f3ad539c778af3c586e45615e81d3cfc992
alibaghani <baghaniali2006@gmail.com>
2025-02-08 06:31:17 -0500

    feat: remove request from abstractAV class and make it global

--- core/utilities.py
+++ core/utilities.py
@@ -1,6 +1,8 @@
 import hashlib
 from pathlib import Path
 
+import requests
+
 
 def calculate_file_hash(uploaded_file)->str:
     hash_obj = hashlib.sha256()
@@ -12,3 +14,17 @@ def calculate_file_hash(uploaded_file)->str:
 
 def get_file_size(file_path: str) -> int:
     return Path(file_path).stat().st_size
+
+
+def request(url, files=None, post: bool = False, **kwargs):
+    """
+    handle request based on its type (post/get)
+
+    """
+    if post:
+        return requests.post(url=url,
+                             files=files,
+                             **kwargs)
+
+    return requests.get(url=url,
+                        **kwargs)
