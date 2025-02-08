d3958df559a55e17f1e60653589ca41b5f43f072
alibaghani <baghaniali2006@gmail.com>
2025-02-08 06:51:34 -0500

    feat: add retrieve method and lock destroy and update methods

--- scanner/views.py
+++ scanner/views.py
@@ -1,6 +1,7 @@
+from rest_framework.exceptions import MethodNotAllowed
+from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from rest_framework import serializers
 from .models import ScanFile
 from .serializers import ScanFileCreateSerializer
 
@@ -9,6 +10,22 @@ class ScanView(ModelViewSet):
     model = ScanFile
     serializer_class = ScanFileCreateSerializer
     queryset = ScanFile.objects.all()
+    lookup_field = 'sha_256'
 
 
+    def retrieve(self, request, *args, **kwargs):
+        try:
+            obj = self.get_queryset().get(sha_256=kwargs['sha_256'])
+            if not obj.status:
+                return Response({"status": "queued"})
+        except ScanFile.DoesNotExist:
+            raise NotFound()
 
+        serializer = self.get_serializer(obj)
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        raise MethodNotAllowed('not allowed')
+
+    def update(self, request, *args, **kwargs):
+        raise MethodNotAllowed('not allowed')
