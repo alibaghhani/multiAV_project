a638d4c1186bf6357a7baf1b388b5e58d42b872f
alibaghani <baghaniali2006@gmail.com>
2025-02-08 06:26:58 -0500

    feat: create manager for scan model
    create anager with a method for bulk create scan instance based on number of avs

--- scanner/managers.py
+++ scanner/managers.py
@@ -1,6 +1,7 @@
-from core.utilities import get_file_size, calculate_file_hash
+from core.utilities import calculate_file_hash
 from django.conf import settings
 from django.db import models
+from services.av_settings import AVS
 
 
 class ScanFileManager(models.Manager):
@@ -12,3 +13,21 @@ class ScanFileManager(models.Manager):
         kwargs['file_size'] = file.size
 
         return super().create(**kwargs)
+
+
+
+class ScanManager(models.Manager):
+
+
+    def bulk_create_scan_for_av_in_avs(self, file_obj):
+        data = []
+        for av in AVS:
+            data.append({'av_name': av.split('.')[2],
+                         'status': 1,
+                         'file': file_obj})
+
+        instances = [self.model(av_name=data['av_name'],
+                          status=data['status'],
+                          file=data['file']) for data in data]
+
+        self.model.objects.bulk_create(instances)
