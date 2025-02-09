f2e56c545cdd2105b6947f552315407495bc55d6
alibaghani <baghaniali2006@gmail.com>
2025-02-09 09:28:20 -0500

    refactor: change upload_file_to_av task functionality
    pass file hash and its name to upload_file_to_av task to avoid extra query

--- services/tasks.py
+++ services/tasks.py
@@ -1,22 +1,22 @@
 from celery import shared_task
 from core.utilities import request, get_nested_value
 from scanner.models import ScanFile, Scan
+from django.db.models import Q
+from django.conf import settings
 
 
 @shared_task(serializer='json')
 def upload_file_to_av(**kwargs):
-    file = ScanFile.objects.get(sha_256=kwargs['file_hash'])
 
-    with open(f'media/{file.file.name}', 'rb') as file:
+    with open(settings.MEDIA_ROOT+kwargs['file_name'], 'rb') as file:
         response = request(url=kwargs['url'] + kwargs['suffix'],
                                 files={'file': file},
                                 post=True)
 
     if response.status_code == 200:
-        parsed_response = response.json()
-        scan = Scan.objects.get(file=file)
+        scan = Scan.objects.get(Q(file__sha_256=kwargs['file_hash'])|Q(av_name=kwargs['av_name']))
         scan.status = 2
-        scan.tracking_id = get_nested_value(parsed_response, kwargs['route'])
+        scan.tracking_id = get_nested_value(response.json(), kwargs['route'])
         scan.save()
     else:
         raise Exception(f"File upload failed with status code {response.status_code}: {response.text}")
