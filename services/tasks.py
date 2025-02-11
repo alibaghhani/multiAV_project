4b95197fa4cee8475dfda3f2ee76bafb1624b01f
alibaghani <baghaniali2006@gmail.com>
2025-02-11 06:15:45 -0500

    refactor: change upload_file task functionality

--- services/tasks.py
+++ services/tasks.py
@@ -1,25 +1,37 @@
+from datetime import datetime
+
 from celery import shared_task
-from core.utilities import request, get_nested_value
-from scanner.models import ScanFile, Scan
-from django.db.models import Q
 from django.conf import settings
+from django.db.models import Q
+
+from core.exceptions import FileUploadError
+from scanner.models import Scan
+from .scanners import VirusTotal
+
+RATE_LIMIT_PERIOD_COUNT = getattr(settings, "RATE_LIMIT_PERIOD_COUNT", 2)
 
 
 @shared_task(serializer='json')
-def upload_file_to_av(**kwargs):
-
-    with open(settings.MEDIA_ROOT+kwargs['file_name'], 'rb') as file:
-        response = request(url=kwargs['url'] + kwargs['suffix'],
-                                files={'file': file},
-                                post=True)
-
-    if response.status_code == 200:
-        scan = Scan.objects.get(Q(file__sha_256=kwargs['file_hash'])|Q(av_name=kwargs['av_name']))
-        scan.status = 2
-        scan.tracking_id = get_nested_value(response.json(), kwargs['route'])
-        scan.save()
-    else:
-        raise Exception(f"File upload failed with status code {response.status_code}: {response.text}")
+def upload_file_to_virustotal():
+    scan_file_objs = Scan.objects.select_related('file').filter(Q(status=1)|Q(av_name='VirusTotal')).order_by('-checked_at')[:RATE_LIMIT_PERIOD_COUNT]
+    for scan_file_obj in scan_file_objs:
+        if not scan_file_obj:
+            try:
+                with open(settings.MEDIA_ROOT+scan_file_obj.file.file.name, 'rb') as file:
+                    vt_obj = VirusTotal(file)
+                    try:
+                        tracking_id = vt_obj.upload_file()
+                        print("tracking id is"+tracking_id)
+                        scan_file_obj.tracking_id = tracking_id
+                        scan_file_obj.status = 2
+                        scan_file_obj.checked_at = datetime.now()
+                        scan_file_obj.save()
+                    except FileUploadError:
+                        continue
+            except NotADirectoryError:
+                continue
+        else:
+            break
 
 
 
