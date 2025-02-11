f91748a07cdc5948b9815e4a7ed0ecd366c2f374
alibaghani <baghaniali2006@gmail.com>
2025-02-11 06:16:03 -0500

    refactor: update celery configration

--- multiav_project/celery.py
+++ multiav_project/celery.py
@@ -4,4 +4,8 @@ from celery import Celery
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "multiav_project.settings")
 app = Celery("multiav")
 app.config_from_object("django.conf:settings", namespace="CELERY")
+app.conf.update(
+    task_concurrency=2,
+    worker_prefetch_multiplier=1
+)
 app.autodiscover_tasks()
