4cf01d9e6bb02cbb1eff3d82830d76e17054f37f
alibaghani <baghaniali2006@gmail.com>
2025-02-12 16:03:48 -0500

    refactor: change tasks functionalities
    change tasks functionalities and remove duplicate codes

--- services/tasks.py
+++ services/tasks.py
@@ -1,64 +1,49 @@
-from datetime import datetime
 import ast
+from datetime import datetime
 from celery import shared_task
 from django.conf import settings
-from django.db.models import Q
 from core.exceptions import FileUploadError, GetFileResultError
-from scanner.models import Scan
 from .scanners import VirusTotal
+from .utils import ScanManager, FileManager
+
 
 RATE_LIMIT_PERIOD_COUNT = getattr(settings, "RATE_LIMIT_PERIOD_COUNT", 2)
 
 
 @shared_task(serializer='json')
 def upload_file_to_virustotal():
-    scan_file_objs = Scan.objects.filter(
-        Q(status=1)&Q(
-        av_name='VirusTotal')).order_by(
-        '-checked_at').select_related(
-        'file')[:RATE_LIMIT_PERIOD_COUNT]
-
-
-    for scan_file_obj in scan_file_objs:
-        if scan_file_obj.status == 1:
-            try:
-                with open(settings.MEDIA_ROOT+scan_file_obj.file.file.name, 'rb') as file:
-                    vt_obj = VirusTotal()
-                    try:
-                        tracking_id = vt_obj.upload_file(file)
-                        scan_file_obj.tracking_id = tracking_id
-                        scan_file_obj.status = 2
-                        scan_file_obj.checked_at = datetime.now()
-                        scan_file_obj.save()
-                    except FileUploadError:
-                        continue
-            except NotADirectoryError:
-                continue
-        else:
-            break
-
+    manager = ScanManager('VirusTotal')
+    for scan_obj in manager.get_scan_records(status=1):
+        try:
+            with open(settings.MEDIA_ROOT + scan_obj.file.file.name, 'rb') as file:
+                vt_obj = VirusTotal()
+                try:
+                    tracking_id = vt_obj.upload_file(file)
+                    manager.update_scan_status(scan_obj=scan_obj, tracking_id=tracking_id, status=2,
+                                               checked_at=datetime.now())
+                except FileUploadError:
+                    continue
+        except NotADirectoryError:
+            continue
 
 
 @shared_task(serializer='json')
 def get_file_scan_result_virustotal():
-    scan_file_objs = Scan.objects.select_related(
-        'file').filter(
-        Q(status=2) & Q(
-        av_name='VirusTotal')).order_by(
-        '-checked_at')[:RATE_LIMIT_PERIOD_COUNT]
+    manager = ScanManager('VirusTotal')
+    file_manager = FileManager()
 
-
-    for scan_file_obj in scan_file_objs:
-        if  scan_file_obj.status == 2:
+    for scan_obj in manager.get_scan_records(status=2):
+        if scan_obj.status == 2:
             vt_obj = VirusTotal()
             try:
-                response = vt_obj.get_results(scan_file_obj.tracking_id)
-                if isinstance(response, str):
-                    dict_response = ast.literal_eval(response)
-                dict_response = response
-                scan_file_obj.status =  3
-                scan_file_obj.short_result, scan_file_obj.final_result = vt_obj.analysis_report(dict_response)
-                scan_file_obj.save()
+                response = vt_obj.get_results(scan_obj.tracking_id)
+                dict_response = ast.literal_eval(response) if isinstance(response, str) else response
+
+                short_result, final_result = vt_obj.analysis_report(dict_response)
+                manager.update_scan_status(scan_obj=scan_obj, status=3, short_result=short_result,
+                                           final_result=final_result)
+                file_manager.save_file_result(scan_obj.file)
+
             except GetFileResultError:
                 continue
 
