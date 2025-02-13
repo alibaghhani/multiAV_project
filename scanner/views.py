eaa4472424ea4b67d5efa87f0c70f71d8bb20e9b
alibaghani <baghaniali2006@gmail.com>
2025-02-13 14:27:17 -0500

    feat: implement scan's viewset

--- scanner/views.py
+++ scanner/views.py
@@ -1,11 +1,10 @@
 from types import NoneType
-
 from rest_framework.exceptions import MethodNotAllowed
 from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet
-from .models import ScanFile
-from .serializers import ScanFileCreateSerializer, ScanFileRetrieveSerializer
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+from .models import ScanFile, Scan
+from .serializers import ScanFileCreateSerializer, ScanFileRetrieveSerializer, ScanDetailSerializer, ScanListSerializer
 
 
 class ScanFileViewSet(ModelViewSet):
@@ -38,3 +37,29 @@ class ScanFileViewSet(ModelViewSet):
 
     def update(self, request, *args, **kwargs):
         raise MethodNotAllowed('not allowed')
+
+
+class ScanViewSet(ReadOnlyModelViewSet):
+
+    def get_serializer_class(self):
+        """Use different serializers for list and retrieve actions."""
+        if self.action == "retrieve":
+            return ScanDetailSerializer
+        return ScanListSerializer
+
+    def get_queryset(self):
+        """Get all scans related to a specific file"""
+        sha_256 = self.kwargs.get("file_sha_256")
+        return Scan.objects.filter(file__sha_256=sha_256)
+
+    def retrieve(self, request, *args, **kwargs):
+        """Get a specific scan by AV name"""
+        sha_256 = self.kwargs.get("file_sha_256")
+        av_name = kwargs.get("pk")
+
+        try:
+            scan = Scan.objects.get(file__sha_256=sha_256, av_name=av_name)
+            serializer = self.get_serializer(scan)
+            return Response(serializer.data)
+        except Scan.DoesNotExist:
+            raise NotFound(f"No scan found for {av_name}")
